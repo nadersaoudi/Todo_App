b2f959f7e7feac579462c8e1d2a48fb3b0b4b876
saoudinader <saoudi.nader7@gmail.com>
2022-07-17 00:22:29 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Reference
+
+    - Create react app:https://create-react-app.dev/
+    - React beautiful dnd: https://github.com/atlassian/react-beautiful-dnd/
+    - Material-UI: https://mui.com/
+    - Express: https://expressjs.com/
---
+++ server/src/v1/controllers/user.js
@@ -0,0 +1,67 @@
+const User = require("../models/user");
+const CryptoJs = require("crypto-js");
+const jsonwebtoken = require("jsonwebtoken");
+const { findOne } = require("../models/user");
+
+//Register User
+exports.register = async (req, res) => {
+  const { password } = req.body;
+  try {
+    req.body.password = CryptoJs.AES.encrypt(
+      password,
+      process.env.PASSWORD_SECRET_KEY
+    );
+    const user = await User.create(req.body);
+    const token = jsonwebtoken.sign(
+      {
+        id: user._id,
+      },
+      process.env.TOKEN_SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+    res.status(201).json({ user, token });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+//login User
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await findOne({ username }).select("password");
+    if (!user) {
+      return res.status(401).json({
+        errors: {
+          params: "username",
+          msg: "Invalid Username or Password",
+        },
+      });
+    }
+
+    const decryptPassword = CryptoJs.AES.decrypt(
+      user.password,
+      process.env.PASSWORD_SECRET_KEY
+    ).toString(CryptoJs.enc.Utf8);
+    if (decryptPassword !== password) {
+      return res.status(401).json({
+        errors: {
+          params: "username",
+          msg: "Invalid Username or Password",
+        },
+      });
+    }
+
+    user.password = undefined;
+
+    const token = jsonwebtoken.sign(
+      {
+        id: user._id,
+      },
+      process.env.TOKEN_SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ user, token });
+  } catch (err) {}
+};
