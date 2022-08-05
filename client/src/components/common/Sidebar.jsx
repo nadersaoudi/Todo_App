559f408b9a453fa0ea7bf018b66c80fe12037c67
saoudinader <saoudi.nader7@gmail.com>
2022-08-05 11:26:25 +0100

    ADD:Boards

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -1,14 +1,51 @@
-import { Drawer, IconButton, List, ListItem, Typography } from "@mui/material";
+import { Drawer, IconButton, List, ListItem, ListItemButton, Typography } from "@mui/material";
+import { useEffect, useState } from 'react'
 import { Box } from "@mui/system";
 import { useSelector, useDispatch } from "react-redux";
 import asset from "../../assets";
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import boardApi from '../../api/boardApi'
+import { setBoards } from '../../redux/features/boadSlice'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+
 const Sidebar = () => {
   const user = useSelector((state) => state.user.value);
+  const boards = useSelector((state) => state.board.value)
+  const dispatch = useDispatch()
+  const { boardId } = useParams()
   const sidebarWidth = 250;
   const navigate = useNavigate();
+  const [activeIndex, setActiveIndex] = useState(0)
+
+  useEffect(() => {
+    const getBoards = async () => {
+      try {
+        const res = await boardApi.getAll()
+        dispatch(setBoards(res))
+       
+      } catch (err) {
+        alert(err)
+      }
+    }
+    getBoards()
+  }, [dispatch])
+
+  useEffect(() => {
+    const activeItem = boards.findIndex(e => e.id === boardId)
+    if (boards.lenght > 0 && boardId === undefined) {
+      navigate(`/boards/${boards[0].id}`)
+    }
+    setActiveIndex(activeItem)
+  }, [boards, boardId, navigate])
+
+
+
+  const onDragEnd = () => {
+
+  }
+
   const logout = () => {
     localStorage.removeItem("token");
     navigate("/login");
@@ -86,6 +123,43 @@ const Sidebar = () => {
             </Box>
           </ListItem>
         </Box>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable key={'list-board-droppable-key'} droppableId={'list-board-droppable'}>
+            {(provided) => (
+              <div ref={provided.innerRef} {...provided.droppableProps}>
+                {
+                  boards.map((item, index) => (
+                    <Draggable key={item.id} draggableId={item.id} index={index}>
+                      {(provided, snapshot) => (
+                        <ListItemButton
+                          ref={provided.innerRef}
+                          {...provided.dragHandleProps}
+                          {...provided.draggableProps}
+                          selected={index === activeIndex}
+                          component={Link}
+                          to={`/boards/${item.id}`}
+                          sx={{
+                            pl: '20px',
+                            cursor: snapshot.isDragging ? 'grab' : 'pointer!important'
+                          }}
+                        >
+                          <Typography
+                            variant='body2'
+                            fontWeight='700'
+                            sx={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
+                          >
+                            {item.icon} {item.title}
+                          </Typography>
+                        </ListItemButton>
+                      )}
+                    </Draggable>
+                  ))
+                }
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       </List>
     </Drawer>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,36 @@
 import { Box } from "@mui/material";
+import { useState } from 'react'
 import React from "react";
 import LoadingButton from "@mui/lab/LoadingButton";
-
+import { useDispatch } from 'react-redux'
+import { setBoards } from '../redux/features/boadSlice'
+import { useNavigate } from 'react-router-dom'
+import boardApi from "../api/boardApi";
 const Home = () => {
-  const createBoard = () => {};
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(false)
+  const createBoard = async () => {
+    try {
+      const res = await boardApi.create()
+      dispatch(setBoards([res]))
+      navigate(`/boards/${res.id}`)
+    } catch (err) {
+      alert(err)
+    } finally {
+      setLoading(false)
+    }
+  };
   return (
     <Box
       sx={{
-        height: "100%",
+        height: "100vh",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
       }}
     >
-      <LoadingButton variant="outlined" color="info" onClick={createBoard}>
+      <LoadingButton variant="outlined" color="info" onClick={createBoard} loading={loading}>
         Click here to create your first board
       </LoadingButton>
     </Box>
